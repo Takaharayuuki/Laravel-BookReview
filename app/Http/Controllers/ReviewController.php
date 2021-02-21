e906acbea382b0d5ddfbd144275ddeec9a87bb6f
Takaharayuuki <takahara.yuuki22@gmail.com>
2021-02-21 18:03:48 +0900

    Add: 画像を選択しなくてもエラーが出ないように修正

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -20,8 +20,12 @@ class ReviewController extends Controller
     public function store(Request $request)
     {
         $post = $request->all();
-        $request->file('image')->store('/public/images');
-        $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];
+        if ($request->hasFile('image')) {
+            $request->file('image')->store('/public/images');
+            $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];
+        } else {
+            $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+        }
 
         Review::insert($data);
 
