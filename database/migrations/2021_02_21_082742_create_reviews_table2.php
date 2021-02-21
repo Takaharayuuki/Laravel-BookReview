2df87e0cefaffb47e0d827e7a749b90a651eae0d
Takaharayuuki <takahara.yuuki22@gmail.com>
2021-02-21 17:32:38 +0900

    レビューテーブルの作成

--- database/migrations/2021_02_21_082742_create_reviews_table2.php
+++ database/migrations/2021_02_21_082742_create_reviews_table2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReviewsTable extends Migration
+class CreateReviewsTable2 extends Migration
 {
     /**
      * Run the migrations.
@@ -14,6 +14,7 @@ class CreateReviewsTable extends Migration
     public function up()
     {
         Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->string('title');
