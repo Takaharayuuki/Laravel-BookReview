cc7519693f0e2153aa0b4c4999fc403c08d0c139
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-22 14:03:40 +0900

    Add:　サムネイルの画像を表示する

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,20 +6,24 @@
 
 @section('content')
 <div class="row justify-content-center">
-
     @foreach ($reviews as $review)
     <div class="col-md-4">
         <div class="card mb50">
             <div class="card-body">
+                @if(!empty($review->image))
+                <div class="image-wrapper">
+                    <img class="book-image" src="{{ asset('storage/images/' . $review->image)}}" alt="">
+                </div>
+                @else
                 <div class="image-wrapper">
                     <img class="book-image" src="{{ asset('images/dummy.png')}}" alt="">
-
-                    <h3 class="h3 book-title">{{ $review->title }}</h3>
-                    <p class="description">
-                        {{ $review->body }}
-                    </p>
-                    <a href="" class="btn btn-secondary detail-btn">詳細を読む</a>
                 </div>
+                @endif
+                <h3 class="h3 book-title">{{ $review->title }}</h3>
+                <p class="description">
+                    {{ $review->body }}
+                </p>
+                <a href="" class="btn btn-secondary detail-btn">詳細を読む</a>
             </div>
         </div>
     </div>
